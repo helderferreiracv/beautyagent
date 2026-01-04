2db5b6ccbef5154611e848fe5416a6019ba41233
helderferreiracv <65920016+helderferreiracv@users.noreply.github.com>
2026-01-04 01:27:21 +0000

    Update Landing.tsx

--- pages/Landing.tsx
+++ pages/Landing.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import LandingContent from "../components/LandingContent";
+import { LandingContent } from "../components/LandingContent";
 
 export default function Landing() {
   return <LandingContent />;
